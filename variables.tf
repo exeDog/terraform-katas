c9962cbefb935d482860ee81421db8dcfa84845f
Priyank Rege <priyank.rege@bigcommerce.com>
2023-09-30 20:27:05 -0500

    variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "vpn_ip" {
+  description = "VPN IP"
+  default     = "116.50.30.20/32"
+}
+
+variable "github_token" {
+  description = "Github Token"
+  default     = "ghp_1234567890"
+}
+
+variable "github_organization" {
+  description = "Github Username"
+  default     = "exedog"
+}
